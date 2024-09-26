5aef667d1f376f9ac5a29f764949f30925d52904
rajibofficial1999 <rajibofficial1999@gmail.com>
2024-09-26 20:02:40 +0600

    chages

--- backend/app/Http/Controllers/Api/AccountInformationController.php
+++ backend/app/Http/Controllers/Api/AccountInformationController.php
@@ -4,13 +4,14 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Interfaces\AccountManage;
+use App\Models\AccountInformation;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
 class AccountInformationController extends Controller
 {
-    public function __construct(protected AccountManage $accountService){}
+    public function __construct(protected AccountManage $accountService) {}
 
     public function store(Request $request): JsonResponse
     {
@@ -19,6 +20,13 @@ class AccountInformationController extends Controller
         $responseCode = $data['success'] ? Response::HTTP_OK : Response::HTTP_UNPROCESSABLE_ENTITY;
 
         return response()->json($data, $responseCode);
+    }
+
+    public function show(string $access_token): JsonResponse
+    {
+        $account = AccountInformation::findByAccessToken($access_token);
+
+        return response()->json($account, Response::HTTP_OK);
     }
 
     public function update(Request $request): JsonResponse
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,18 +2,15 @@
 
 use App\Http\Controllers\Api\VisitorInformationController;
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AccountInformationController;
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
 
 Route::prefix('v1')->group(function () {
     Route::post('/accounts/store', [AccountInformationController::class, 'store']);
 
     Route::post('/accounts/update', [AccountInformationController::class, 'update']);
+
+    Route::get('/accounts/show-account/{access_token}', [AccountInformationController::class, 'show']);
 
     Route::post('/visitor-information/store', [VisitorInformationController::class, 'store']);
 });
