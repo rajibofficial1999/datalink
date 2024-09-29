6a01debe9d2e0804045ac814e12f9695513cdd21
Rajib Ha San <rajibofficial1999@gmail.com>
2024-09-30 00:11:13 +0600

    add some files

---
+++ fontend/src/utils/store/errorsSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const errorsSlice = createSlice({
+  name: 'errors',
+  initialState: {
+    value: {}
+  },
+  reducers: {
+    addErrors: (state, action) => {
+      console.log(action.payload)
+      // state.value = action.payload;
+    },
+  },
+});
+
+export const { addErrors } = errorsSlice.actions;
+
+export default errorsSlice.reducer;
